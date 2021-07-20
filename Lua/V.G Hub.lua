fa45eb70b4f97a4cae89a40048ca447d7a1786a2
創真 <37291660+Hakame-kun@users.noreply.github.com>
2021-07-20 21:46:38 +0700

    Create scripts

---
+++ Lua/V.G Hub.lua
@@ -0,0 +1,64 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub', true))()
+
+-- Features:
+-- Adopt me
+-- Age Of Heros
+-- Arsenal
+-- Assassin
+-- Bad Business
+-- Be Crushed by a Speeding Wall
+-- Blox Fruits
+-- Break-in-Story
+-- Breaking Point
+-- Bubble Gun Simulator
+-- Build a boat
+-- Car Crusher 2
+-- Climb 1,000 Stairs
+-- Cursed Islands
+-- DemonFall
+-- Destruction Simulator
+-- Dragon Adventures
+-- Dungeon Quest!
+-- Elemental Battlegrounds
+-- Elemental Legends
+-- Field Trip Z
+-- GreenVile
+-- Jailbreak
+-- KAT
+-- Knife Simulator
+-- Legends Of Speed
+-- Little World
+-- MM2
+-- Magnet Simulator
+-- Meep City
+-- Mega Easy Obby
+-- Muscle Legends
+-- Natural Disaster Survival
+-- Ninja Legends
+-- Ninja Legends 2
+-- Phantom Forces
+-- Project Lazarus:ZOMBIES
+-- Project XL
+-- Ragdoll Engine
+-- Shindo
+-- SHOOT OUT!
+-- Soda Champions
+-- Speed Run 4
+-- Super Bomb Sirvival
+-- Survive The Disasters! Classic
+-- Taping-Gods
+-- The Asylum
+-- The Floor Is LAVA
+-- The Labyrinth
+-- The Maze
+-- Timber
+-- Tower of hell
+-- Trade Tower
+-- TSUNAMI SURVIVAL
+-- Vehicle Legends
+-- Vehicle Simulator
+-- Warrior Champions
+-- Weaponry
+-- Zombies Attack
+-- Zombie Rush
+-- Zombie Uprising

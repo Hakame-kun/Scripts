4df1fc0772a0ecbdc61d4d774bac49bc90cb9866
創真 <37291660+Hakame-kun@users.noreply.github.com>
2021-07-21 19:03:23 +0700

    Anti AFK Removed
    You may need to execute Anti AFK.lua. This Hub auto execute if you disconnected/reconnected

--- Lua/V.G Hub.lua
+++ Lua/V.G Hub.lua
@@ -1,8 +1,3 @@
--- Anti AFK
-for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
-v:Disable()
-end
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub", true))()
 
 -- Features:

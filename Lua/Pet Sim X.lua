e6c61288d55c303197121faaa49f790c9fe51664
創真 <37291660+Hakame-kun@users.noreply.github.com>
2021-08-28 07:38:24 +0700

    Update Pet Sim X.lua

--- Lua/Pet Sim X.lua
+++ Lua/Pet Sim X.lua
@@ -1,3 +1,3 @@
+loadstring(game:HttpGet("https://github.com/Hakame-kun/Scripts/raw/main/Pet%20Sim%20X/Pet%20Sim%20X%20Main.lua", true))()
 loadstring(game:HttpGet("https://github.com/Hakame-kun/Scripts/raw/main/Pet%20Sim%20X/Pet%20Sim%20X%20Stats.lua", true))()
 loadstring(game:HttpGet("https://github.com/Hakame-kun/Scripts/raw/main/Pet%20Sim%20X/Pet%20Sim%20X%20Fuse.lua", true))()
-loadstring(game:HttpGet("https://github.com/Hakame-kun/Scripts/raw/main/Pet%20Sim%20X/Pet%20Sim%20X%20Main.lua", true))()
